f1dd49715f12c0b0fcd5df1915ced7b04a3dd31f
yinjiehu <576813016@qq.com>
2017-08-17 15:37:43 +0800

    Add Project files

---
+++ Assets/AssetBundleKit/Editor/AssetBundleWindow.cs
@@ -0,0 +1,27 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace IronFury
+{
+	public class AssetBundleWindow : EditorWindow
+	{
+		[MenuItem("Tools/Publish AssetBundle")]
+		static void ShowWindow()
+		{
+			GetWindow<AssetBundleWindow>().Show();
+		}
+
+		Vector2 scrollPosition;
+
+		void OnGUI()
+		{
+			EditorGUILayout.BeginScrollView(scrollPosition, GUILayout.MinWidth(300), GUILayout.Height(300));
+			string sss = "ddd";
+			EditorGUILayout.LabelField(sss);
+			EditorGUILayout.LabelField(sss);
+			EditorGUILayout.LabelField(sss);
+			EditorGUILayout.EndScrollView();
+			GUILayout.Label("scene id build");
+		}
+    }
+}
